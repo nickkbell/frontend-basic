312414c0fcee27ebd8bc232bc5cee9115f63f9bf
nick <n.belitskiy@infomir.com>
2025-02-25 11:25:18 +0200

    add js plugin

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 6500,
+        autoplaySpeed: 1000,
+        loop: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            1100:{
+                items:1
+            }
+        }
+    });
+});
