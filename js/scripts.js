de19ec9543d373ae121d11b86ffe8f34f5238621
nick <n.belitskiy@infomir.com>
2025-03-03 13:58:06 +0200

    add adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.first-carousel').owlCarousel({
         autoplay: true,
         autoplayTimeout: 6500,
         autoplaySpeed: 1000,
         loop: true,
+        rewind: true,
+        autoWidth: false,
+        dots: true,
         responsive:{
             0:{
                 items:1
             },
             1100:{
-                items:1
+                items:2
+            }
+        }
+    });
+
+    $('.second-carousel').owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 6500,
+        autoplaySpeed: 1000,
+        loop: true,
+        autoWidth: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            1100: {
+                items: 4
             }
         }
     });
 });
+
